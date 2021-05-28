798a11f8c5833ae74026f60b7115ee3a9542b4b2
1z3r091 <1z3r091@gmail.com>
2021-05-28 19:56:07 +0900

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Link from "next/link";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <div className="container">
       <Head>
@@ -10,42 +12,55 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          블로그
         </h1>
+        
+          <ol>
+            <li>  
+              <Link href="">
+                  <a>페이지</a>
+              </Link>
+            </li>
+            <li>  
+              <Link href="">
+                  <a>페이지</a>
+              </Link>
+            </li>
+            <li>  
+              <Link href="">
+                  <a>페이지</a>
+              </Link>
+            </li>
+            <li>  
+              <Link href="">
+                  <a>페이지</a>
+              </Link>
+            </li>
+            <li>  
+              <Link href="">
+                  <a>페이지</a>
+              </Link>
+            </li>
+            <li>  
+              <Link href="">
+                  <a>페이지</a>
+              </Link>
+            </li>
+            <li>  
+              <Link href="">
+                  <a>페이지</a>
+              </Link>
+            </li>
+          </ol>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section>
+          <h2>get data test</h2>
+          <ul>
+            {allPostsData.map(({ id, date, title }) => (
+              <div>test</div>
+            ))}
+          </ul>
+        </section>
       </main>
 
       <footer>
@@ -54,7 +69,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +220,14 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
