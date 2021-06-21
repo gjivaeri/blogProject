ddcf88b4a35cf29e6a39d03a1722be70db3c2470
1z3r091 <1z3r091@gmail.com>
2021-06-21 20:25:51 +0900

    fix/header&sidebar_css

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -2,20 +2,22 @@
 import styles from "./sidebar.module.css";
 import Link from "next/link";
 
-
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <div className={styles.sidebarItem}>
         <span className={styles.sidebarTitle}>ABOUT ME</span>
-        <img
-          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
-          alt=""
-        />
-        <p>
-          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
-          amet ex esse.Sunt eu ut nostrud id quis proident no meaning words.
-        </p>
+
+        <div className={styles.sidebarContent}>
+          <p>
+            Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+            amet ex esse.Sunt eu ut nostrud id quis proident no meaning words.
+          </p>
+          <img
+            src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+            alt=""
+          />
+        </div>
       </div>
       <div className={styles.sidebarItem}>
         <span className={styles.sidebarTitle}>CATEGORIES</span>
